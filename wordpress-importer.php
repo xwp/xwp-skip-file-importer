af915383a422ad367b924f68d8d7360c4b3e5917
Kaspars Dambis <hi@kaspars.net>
2016-10-30 13:10:35 +0200

    Try to fetch the remote file if not found locally

--- wordpress-importer.php
+++ wordpress-importer.php
@@ -984,27 +984,66 @@ class XWP_Skip_Files_Importer extends WP_Importer {
 		$new_file = sprintf( '%s/%s', $upload_dir['path'], $file_name );
 		$new_file_url = sprintf( '%s/%s', $upload_dir['url'], $file_name );
 
-		if ( ! file_exists( $new_file ) ) {
-			return new WP_Error( 'upload_file_missing',
-				sprintf(
-					'Failed to find %s on the server.',
-					$new_file
-				)
+		if ( file_exists( $new_file ) ) {
+			// Use the existing file
+			$wp_filetype = wp_check_filetype( $new_file );
+
+			$upload = array(
+				'file' => $new_file,
+				'url' => $new_file_url,
+				'type' => $wp_filetype['type'],
+				'error' => false
 			);
+
+			// keep track of the old and new urls so we can substitute them later
+			$this->url_remap[$url] = $upload['url'];
+			$this->url_remap[$post['guid']] = $upload['url']; // r13735, really needed?
+		} else {
+			// get placeholder file in the upload dir with a unique, sanitized filename
+			$upload = wp_upload_bits( $file_name, 0, '', $post['upload_date'] );
+			if ( $upload['error'] )
+				return new WP_Error( 'upload_dir_error', $upload['error'] );
+
+			// fetch the remote url and write it to the placeholder file
+			$headers = wp_get_http( $url, $upload['file'] );
+
+			// request failed
+			if ( ! $headers ) {
+				@unlink( $upload['file'] );
+				return new WP_Error( 'import_file_error', __('Remote server did not respond', 'wordpress-importer') );
+			}
+
+			// make sure the fetch was successful
+			if ( $headers['response'] != '200' ) {
+				@unlink( $upload['file'] );
+				return new WP_Error( 'import_file_error', sprintf( __('Remote server returned error response %1$d %2$s', 'wordpress-importer'), esc_html($headers['response']), get_status_header_desc($headers['response']) ) );
+			}
+
+			$filesize = filesize( $upload['file'] );
+
+			if ( isset( $headers['content-length'] ) && $filesize != $headers['content-length'] ) {
+				@unlink( $upload['file'] );
+				return new WP_Error( 'import_file_error', __('Remote file is incorrect size', 'wordpress-importer') );
+			}
+
+			if ( 0 == $filesize ) {
+				@unlink( $upload['file'] );
+				return new WP_Error( 'import_file_error', __('Zero size file downloaded', 'wordpress-importer') );
+			}
+
+			$max_size = (int) $this->max_attachment_size();
+			if ( ! empty( $max_size ) && $filesize > $max_size ) {
+				@unlink( $upload['file'] );
+				return new WP_Error( 'import_file_error', sprintf(__('Remote file is too large, limit is %s', 'wordpress-importer'), size_format($max_size) ) );
+			}
+
+			// keep track of the old and new urls so we can substitute them later
+			$this->url_remap[$url] = $upload['url'];
+			$this->url_remap[$post['guid']] = $upload['url']; // r13735, really needed?
+			// keep track of the destination if the remote url is redirected somewhere else
+			if ( isset($headers['x-final-location']) && $headers['x-final-location'] != $url )
+				$this->url_remap[$headers['x-final-location']] = $upload['url'];
 		}
-
-		$wp_filetype = wp_check_filetype( $new_file );
-
-		$upload = array(
-			'file' => $new_file,
-			'url' => $new_file_url,
-			'type' => $wp_filetype['type'],
-			'error' => false
-		);
-
-		// keep track of the old and new urls so we can substitute them later
-		$this->url_remap[$url] = $upload['url'];
-		$this->url_remap[$post['guid']] = $upload['url']; // r13735, really needed?
 
 		return $upload;
 	}
